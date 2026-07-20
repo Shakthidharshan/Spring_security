4b106d200568be7fc0195a64759333a413de48e0
Shakthidharshan <shakthisd223@gmail.com>
2026-07-20 10:07:41 +0530

    Update Controller.java

--- Springsecurity/src/main/java/com/shakthi/Springsecurity/Controller.java
+++ Springsecurity/src/main/java/com/shakthi/Springsecurity/Controller.java
@@ -12,6 +12,7 @@ public class Controller {
     public  String greet(HttpServletRequest request){
         System.out.println("welcome to home page");
         System.out.println("this a java project");
+        System.out.println("this a java project");
         return "welcome to home page " + request.getSession().getId();
 
     }
