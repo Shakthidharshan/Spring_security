6d2cf11c63d21b15e39b99822edb6852ea63f5b9
shakthi dharshan <shakthisd223@gmail.com>
2026-07-20 08:36:33 +0530

    First commit

---
+++ Springsecurity/src/main/java/com/shakthi/Springsecurity/Controller.java
@@ -0,0 +1,16 @@
+package com.shakthi.Springsecurity;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public  String greet(HttpServletRequest request){
+        return "welcome to home page " + request.getSession().getId();
+    }
+
+}
